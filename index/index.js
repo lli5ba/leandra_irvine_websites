d87782116f6763ec650ac2d0c34114411e64c240
Leandra <lli5ba@virginia.edu>
2015-07-26 18:54:25 -0400

    Create index.js

---
+++ index/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('i').hover(
+        function() {
+            $(this).css('color', '#00B2FF');
+    },
+        function() {
+            $(this).css('color', 'white');
+                 });
+});
